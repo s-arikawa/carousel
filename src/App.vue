b5c2ecb3de0f926256de5794b7a02a9e44cd46e6
arikawa <arikawa@i3-systems.com>
2020-07-05 14:34:11 +0900

    Carousel 作り始め

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Carousel />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import Carousel from "@/components/Carousel.vue";
 
 @Component({
   components: {
-    HelloWorld
+    Carousel
   }
 })
 export default class App extends Vue {}
